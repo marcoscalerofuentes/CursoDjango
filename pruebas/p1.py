761ef1635551a4c7f156326bd836f9351b0f46d5
marcoscalerofuentes <marcoscaleero@gmail.com>
2023-10-05 19:48:38 +0200

    day1

---
+++ pruebas/p1.py
@@ -0,0 +1,4 @@
+if 3 > 4:
+    print("El primer digito es mayor")
+else:
+    print("El segundo es mayor")
---
+++ pruebas/p2.py
@@ -0,0 +1,10 @@
+print("Introduce un valor")
+num1 = int(input())
+print("Introduce otro valor")
+num2 = int(input())
+if num1 > num2:
+    print("El primer numero es mayor")
+elif num1 < num2:
+    print("El segundo numero es mayor")
+else:
+    print("Son el mismo numero")
