cde5d86a1f92bc56162ff15f2e1b830a0cb3c8f2
marcoscalerofuentes <marcoscaleero@gmail.com>
2023-10-09 09:35:57 +0200

    up

---
+++ pruebas/p3.py
@@ -0,0 +1,14 @@
+if 5 > 2:
+    print("Five is greater than two!") 
+if 5 > 2:
+    print("Five is greater than two!") 
+    #esto es un coment 
+    """esto es un super coment"""
+    x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+#como poner "5john"
+x = 5
+y = "John"
+print(x , y)
